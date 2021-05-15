e92f6adf32cd5ff98829cb1a738c5f4eda8eddd2
AjayLiu <ajayliu2928@gmail.com>
2021-05-15 10:34:26 -0700

    moved context provider to _app so the entire app has access to the socket

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
-
+import io from "socket.io-client";
 import "@styles/globals.scss";
+import SocketContext from "@context/SocketContext";
+const SOCKET_URL =
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:5000";
+
+const socket = io(SOCKET_URL);
+
 function Application({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SocketContext.Provider value={socket}>
+      <Component {...pageProps} />
+    </SocketContext.Provider>
+  );
 }
 
 export default Application;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,19 +1,15 @@
-import io from "socket.io-client";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import SocketContext from "@context/SocketContext";
+import React, { useContext, useEffect, useState } from "react";
 import Footer from "@components/Footer/Footer";
 
 import Header from "@components/Header/Header";
 import Game from "@components/Game/Game";
 import LobbyList from "@components/LobbyList/LobbyList";
-
-const SOCKET_URL =
-  process.env.NODE_ENV === "production" ? "/" : "http://localhost:5000";
-
-const socket = io(SOCKET_URL);
+import SocketContext from "@context/SocketContext";
 
 const Home: React.FC = () => {
+  const socket = useContext(SocketContext);
+
   useEffect(() => {
     socket.on("lobbyJoined", () => {
       setHasJoinedLobby(true);
@@ -21,16 +17,14 @@ const Home: React.FC = () => {
   }, []);
   const [hasJoinedLobby, setHasJoinedLobby] = useState(false);
   return (
-    <div>
+    <>
       <Head>
         <title>Chess Clock Trivia</title>
       </Head>
       <Header />
-      <SocketContext.Provider value={socket}>
-        {hasJoinedLobby ? <Game /> : <LobbyList />}
-      </SocketContext.Provider>
+      {hasJoinedLobby ? <Game /> : <LobbyList />}
       <Footer />
-    </div>
+    </>
   );
 };
 
