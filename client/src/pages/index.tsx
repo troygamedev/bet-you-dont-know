9a3011bcd3bfcf9b903030dfe93d7799c37ed193
AjayLiu <ajayliu2928@gmail.com>
2021-06-02 21:35:39 -0700

    try and add opengraph

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "@components/Layout/Layout";
 const Home: React.FC = () => {
   return (
     <>
-      <Layout title="Bet You Don't Know">
+      <Layout title="Bet You Don't Know" url={window.location.href}>
         <LobbyList />
       </Layout>
     </>
